91e01aafb3fb8992a8075bc759580cf75d83b363
Abisha1303 <abisha.kulfi1997@gmail.com>
2020-07-19 19:06:24 +0530

    changes updated

--- src/oug/test/PractTest.java
+++ src/oug/test/PractTest.java
@@ -3,6 +3,12 @@ package oug.test;
 
 
 public class PractTest {
+
+	public void test3() {
+		System.out.println("java3");
+			}
+	
+	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ja enterprises\\eclipse-workspace\\SeleniumPractice\\driver\\chromedriver.exe");
 		System.out.println("hi this is just a practice");
