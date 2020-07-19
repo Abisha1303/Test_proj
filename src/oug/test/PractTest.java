5731740d2acbcf989919236ee70e28aee8048540
Abisha1303 <abisha.kulfi1997@gmail.com>
2020-07-19 16:41:46 +0530

    commit message

---
+++ src/oug/test/PractTest.java
@@ -0,0 +1,11 @@
+package oug.test;
+
+
+
+public class PractTest {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ja enterprises\\eclipse-workspace\\SeleniumPractice\\driver\\chromedriver.exe");
+		System.out.println("hi this is just a practice");
+		
+	}
+}
